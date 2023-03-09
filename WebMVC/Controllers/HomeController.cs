f682ade20dd41b4cd75be631d622b78963fc5650
Nhuong <nhuongthai8@gmail.com>
2023-03-09 09:03:03 +0700

    TBN_5327

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebMVC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Trang contact.";
 
             return View();
         }
